4a5301f1f78e4356aa4d992a3eb0c2e9f45bb8d1
James Burbage <jeburbage@gmail.com>
2018-11-01 22:26:23 -0600

    Updates

---
+++ precache-manifest.b9e0fe932cadd7686001171cb70fc60c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memoize/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a418a8cbff2e33a9fcc",
+    "url": "/memoize/static/js/runtime~main.1a418a8c.js"
+  },
+  {
+    "revision": "acd74b15d07fd35695ab",
+    "url": "/memoize/static/js/main.acd74b15.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/memoize/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "acd74b15d07fd35695ab",
+    "url": "/memoize/static/css/main.aa1caabe.chunk.css"
+  },
+  {
+    "revision": "653f828fe91c3da634748ba3c0ef0997",
+    "url": "/memoize/index.html"
+  }
+];
